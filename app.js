843a2f8d80983987113bfbf40394dc24c8538df8
Shane Shardlow <shane.shardlow447@airm365.onmicrosoft.com>
2020-03-27 15:54:24 +0000

    completed db env var

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes = require("./routes/comments"),
 
 mongoose.connect(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost:27017/yelp_camp_v4", {useUnifiedTopology: true, useNewUrlParser: true});
-mongoose.connect("mongodb://dba:Passw0rd@ds163360.mlab.com:63360/heroku_w6qlj4s7", {useUnifiedTopology: true, useNewUrlParser: true});
+// mongoose.connect("mongodb://dba:Passw0rd@ds163360.mlab.com:63360/heroku_w6qlj4s7", {useUnifiedTopology: true, useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
