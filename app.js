3e636e7b57ea57bffdd71017c36b0a780790606a
Shane Shardlow <shane.shardlow447@airm365.onmicrosoft.com>
2020-03-27 15:50:03 +0000

    env variables added for db

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
 
+mongoose.connect(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost:27017/yelp_camp_v4", {useUnifiedTopology: true, useNewUrlParser: true});
 mongoose.connect("mongodb://dba:Passw0rd@ds163360.mlab.com:63360/heroku_w6qlj4s7", {useUnifiedTopology: true, useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
